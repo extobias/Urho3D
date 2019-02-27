819e219de8cb9546e0e21565832018ad50fbeb5e
Tobias Alarcon <talarcon@fi.uba.ar>
2019-02-27 20:57:52 -0300

    turbobadger implementation samples units

---
+++ Source/Samples/65_PhysicsTest/Physics.cpp
@@ -0,0 +1,518 @@
+//
+// Copyright (c) 2008-2017 the Urho3D project.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#include <Urho3D/Core/CoreEvents.h>
+#include <Urho3D/Engine/Engine.h>
+#include <Urho3D/Graphics/Camera.h>
+#include <Urho3D/Graphics/DebugRenderer.h>
+#include <Urho3D/Graphics/Graphics.h>
+#include <Urho3D/Graphics/Light.h>
+#include <Urho3D/Graphics/Material.h>
+#include <Urho3D/Graphics/Model.h>
+#include <Urho3D/Graphics/Octree.h>
+#include <Urho3D/Graphics/Renderer.h>
+#include <Urho3D/Graphics/Skybox.h>
+#include <Urho3D/Graphics/Zone.h>
+#include <Urho3D/Input/Input.h>
+#include <Urho3D/IO/File.h>
+#include <Urho3D/IO/Log.h>
+#include <Urho3D/IO/FileSystem.h>
+#include <Urho3D/Physics/CollisionShape.h>
+#include <Urho3D/Physics/PhysicsWorld.h>
+#include <Urho3D/Physics/RigidBody.h>
+#include <Urho3D/Physics/PhysicsEvents.h>
+#include <Urho3D/Resource/ResourceCache.h>
+#include <Urho3D/Scene/Scene.h>
+#include <Urho3D/UI/Font.h>
+#include <Urho3D/UI/Text.h>
+#include <Urho3D/UI/UI.h>
+
+#include "Physics.h"
+
+#include <Urho3D/DebugNew.h>
+
+URHO3D_DEFINE_APPLICATION_MAIN(Physics)
+
+Physics::Physics(Context* context) :
+	Sample(context),
+	drawDebug_(false),
+	mass_(100.0f),
+	length_(3.0f),
+	width_(2.0f),
+	debugText_(nullptr),
+	maxHeight_(0.0f),
+	updateLinSuspension_(false),
+	updateRotSuspension_(false),
+	floatHeight_(2.0f),
+	k_(50.0f),
+	kFactor_(0),
+	maxImpulse_(0.0f),
+	timeElapsed_(0.0f)
+{
+}
+
+void Physics::Start()
+{
+    // Execute base class startup
+    Sample::Start();
+
+    // Create the scene content
+    CreateScene();
+
+    // Create the UI content
+    // CreateInstructions();
+	CreateDebugText();
+    // Setup the viewport for displaying the scene
+    SetupViewport();
+
+    // Hook up to the frame update and render post-update events
+    SubscribeToEvents();
+
+    // Set the mouse mode to use in the sample
+    Sample::InitMouseMode(MM_RELATIVE);
+}
+
+void Physics::CreateScene()
+{
+    ResourceCache* cache = GetSubsystem<ResourceCache>();
+
+    scene_ = new Scene(context_);
+
+    // Create octree, use default volume (-1000, -1000, -1000) to (1000, 1000, 1000)
+    // Create a physics simulation world with default parameters, which will update at 60fps. Like the Octree must
+    // exist before creating drawable components, the PhysicsWorld must exist before creating physics components.
+    // Finally, create a DebugRenderer component so that we can draw physics debug geometry
+    scene_->CreateComponent<Octree>();
+    scene_->CreateComponent<PhysicsWorld>();
+    scene_->CreateComponent<DebugRenderer>();
+
+    // Create a Zone component for ambient lighting & fog control
+    Node* zoneNode = scene_->CreateChild("Zone");
+    Zone* zone = zoneNode->CreateComponent<Zone>();
+    zone->SetBoundingBox(BoundingBox(-1000.0f, 1000.0f));
+    zone->SetAmbientColor(Color(0.15f, 0.15f, 0.15f));
+    zone->SetFogColor(Color(1.0f, 1.0f, 1.0f));
+    zone->SetFogStart(300.0f);
+    zone->SetFogEnd(500.0f);
+
+    // Create a directional light to the world. Enable cascaded shadows on it
+    Node* lightNode = scene_->CreateChild("DirectionalLight");
+    lightNode->SetDirection(Vector3(0.6f, -1.0f, 0.8f));
+    Light* light = lightNode->CreateComponent<Light>();
+    light->SetLightType(LIGHT_DIRECTIONAL);
+    light->SetCastShadows(true);
+    light->SetShadowBias(BiasParameters(0.00025f, 0.5f));
+    // Set cascade splits at 10, 50 and 200 world units, fade shadows out at 80% of maximum shadow distance
+    light->SetShadowCascade(CascadeParameters(10.0f, 50.0f, 200.0f, 0.0f, 0.8f));
+
+    // Create skybox. The Skybox component is used like StaticModel, but it will be always located at the camera, giving the
+    // illusion of the box planes being far away. Use just the ordinary Box model and a suitable material, whose shader will
+    // generate the necessary 3D texture coordinates for cube mapping
+    Node* skyNode = scene_->CreateChild("Sky");
+    skyNode->SetScale(500.0f); // The scale actually does not matter
+    Skybox* skybox = skyNode->CreateComponent<Skybox>();
+    skybox->SetModel(cache->GetResource<Model>("Models/Box.mdl"));
+    skybox->SetMaterial(cache->GetResource<Material>("Materials/Skybox.xml"));
+
+    {
+        // Create a floor object, 1000 x 1000 world units. Adjust position so that the ground is at zero Y
+        Node* floorNode = scene_->CreateChild("Floor");
+        floorNode->SetPosition(Vector3(0.0f, -0.5f, 0.0f));
+        floorNode->SetScale(Vector3(1000.0f, 1.0f, 1000.0f));
+        StaticModel* floorObject = floorNode->CreateComponent<StaticModel>();
+        floorObject->SetModel(cache->GetResource<Model>("Models/Box.mdl"));
+        floorObject->SetMaterial(cache->GetResource<Material>("Materials/StoneTiled.xml"));
+
+        // Make the floor physical by adding RigidBody and CollisionShape components. The RigidBody's default
+        // parameters make the object static (zero mass.) Note that a CollisionShape by itself will not participate
+        // in the physics simulation
+        /*RigidBody* body = */floorNode->CreateComponent<RigidBody>();
+        CollisionShape* shape = floorNode->CreateComponent<CollisionShape>();
+        // Set a box shape of size 1 x 1 x 1 for collision. The shape will be scaled with the scene node scale, so the
+        // rendering and physics representation sizes should match (the box model is also 1 x 1 x 1.)
+        shape->SetBox(Vector3::ONE);
+    }
+
+    {
+        // Create a pyramid of movable physics objects
+        //for (int y = 0; y < 8; ++y)
+        //{
+        //    for (int x = -y; x <= y; ++x)
+        //    {
+		int x = 0, y = 0;
+        Node* boxNode = scene_->CreateChild("Box");
+		boxNode->SetScale(Vector3(width_, 1.0f, length_));
+        boxNode->SetPosition(Vector3((float)x, -(float)y + 2.0f, 0.0f));
+        StaticModel* boxObject = boxNode->CreateComponent<StaticModel>();
+        boxObject->SetModel(cache->GetResource<Model>("Models/Box.mdl"));
+        boxObject->SetMaterial(cache->GetResource<Material>("Materials/StoneEnvMapSmall.xml"));
+        boxObject->SetCastShadows(true);
+
+        // Create RigidBody and CollisionShape components like above. Give the RigidBody mass to make it movable
+        // and also adjust friction. The actual mass is not important; only the mass ratios between colliding
+        // objects are significant
+        body_ = boxNode->CreateComponent<RigidBody>();
+        body_->SetMass(mass_);
+        // body_->SetFriction(0.75f);
+		// body_->SetAngularFactor(Vector3::ZERO);
+		// body_->SetLinearFactor(Vector3::UP);
+        CollisionShape* shape = boxNode->CreateComponent<CollisionShape>();
+        shape->SetBox(Vector3::ONE);
+        //    }
+        //}
+    }
+
+    // Create the camera. Set far clip to match the fog. Note: now we actually create the camera node outside the scene, because
+    // we want it to be unaffected by scene load / save
+    cameraNode_ = new Node(context_);
+    Camera* camera = cameraNode_->CreateComponent<Camera>();
+    camera->SetFarClip(500.0f);
+
+    // Set an initial position for the camera scene node above the floor
+    cameraNode_->SetPosition(Vector3(0.0f, 5.0f, -20.0f));
+}
+
+void Physics::CreateInstructions()
+{
+    ResourceCache* cache = GetSubsystem<ResourceCache>();
+    UI* ui = GetSubsystem<UI>();
+
+    // Construct new Text object, set string to display and font to use
+    Text* instructionText = ui->GetRoot()->CreateChild<Text>();
+    instructionText->SetText(
+        "Use WASD keys and mouse/touch to move\n"
+        "LMB to spawn physics objects\n"
+        "F5 to save scene, F7 to load\n"
+        "Space to toggle physics debug geometry"
+    );
+    instructionText->SetFont(cache->GetResource<Font>("Fonts/Anonymous Pro.ttf"), 5);
+    // The text has multiple rows. Center them in relation to each other
+    instructionText->SetTextAlignment(HA_CENTER);
+
+    // Position the text relative to the screen center
+    instructionText->SetHorizontalAlignment(HA_CENTER);
+    instructionText->SetVerticalAlignment(VA_CENTER);
+    instructionText->SetPosition(0, ui->GetRoot()->GetHeight() / 4);
+}
+
+void Physics::CreateDebugText()
+{
+	ResourceCache* cache = GetSubsystem<ResourceCache>();
+	UI* ui = GetSubsystem<UI>();
+
+	debugText_ = ui->GetRoot()->CreateChild<Text>();
+
+	debugText_->SetFont(cache->GetResource<Font>("Fonts/Anonymous Pro.ttf"), 10);
+	// The text has multiple rows. Center them in relation to each other
+	debugText_->SetTextAlignment(HA_CENTER);
+
+	// Position the text relative to the screen center
+	debugText_->SetHorizontalAlignment(HA_LEFT);
+	debugText_->SetVerticalAlignment(VA_CENTER);
+	debugText_->SetPosition(0, ui->GetRoot()->GetHeight() / 4);
+}
+
+void Physics::SetupViewport()
+{
+    Renderer* renderer = GetSubsystem<Renderer>();
+
+    // Set up a viewport to the Renderer subsystem so that the 3D scene can be seen
+    SharedPtr<Viewport> viewport(new Viewport(context_, scene_, cameraNode_->GetComponent<Camera>()));
+    renderer->SetViewport(0, viewport);
+}
+
+void Physics::SubscribeToEvents()
+{
+    // Subscribe HandleUpdate() function for processing update events
+    SubscribeToEvent(E_UPDATE, URHO3D_HANDLER(Physics, HandleUpdate));
+
+    // Subscribe HandlePostRenderUpdate() function for processing the post-render update event, during which we request
+    // debug geometry
+    SubscribeToEvent(E_POSTRENDERUPDATE, URHO3D_HANDLER(Physics, HandlePostRenderUpdate));
+
+	SubscribeToEvent(E_KEYDOWN, URHO3D_HANDLER(Physics, HandleKeyDown));
+
+	Component* world = scene_->GetComponent<PhysicsWorld>();
+	SubscribeToEvent(world, E_PHYSICSPRESTEP, URHO3D_HANDLER(Physics, HandlePhysicsPreStep));
+}
+
+void Physics::MoveCamera(float timeStep)
+{
+    // Do not move if the UI has a focused element (the console)
+    if (GetSubsystem<UI>()->GetFocusElement())
+        return;
+
+    Input* input = GetSubsystem<Input>();
+
+    // Movement speed as world units per second
+    const float MOVE_SPEED = 20.0f;
+    // Mouse sensitivity as degrees per pixel
+    const float MOUSE_SENSITIVITY = 0.1f;
+
+    // Use this frame's mouse motion to adjust camera node yaw and pitch. Clamp the pitch between -90 and 90 degrees
+    IntVector2 mouseMove = input->GetMouseMove();
+    yaw_ += MOUSE_SENSITIVITY * mouseMove.x_;
+    pitch_ += MOUSE_SENSITIVITY * mouseMove.y_;
+    pitch_ = Clamp(pitch_, -90.0f, 90.0f);
+
+    // Construct new orientation for the camera scene node from yaw and pitch. Roll is fixed to zero
+    cameraNode_->SetRotation(Quaternion(pitch_, yaw_, 0.0f));
+
+    // Read WASD keys and move the camera scene node to the corresponding direction if they are pressed
+    if (input->GetKeyDown(KEY_W))
+        cameraNode_->Translate(Vector3::FORWARD * MOVE_SPEED * timeStep);
+    if (input->GetKeyDown(KEY_S))
+        cameraNode_->Translate(Vector3::BACK * MOVE_SPEED * timeStep);
+    if (input->GetKeyDown(KEY_A))
+        cameraNode_->Translate(Vector3::LEFT * MOVE_SPEED * timeStep);
+    if (input->GetKeyDown(KEY_D))
+        cameraNode_->Translate(Vector3::RIGHT * MOVE_SPEED * timeStep);
+	if (input->GetKeyDown(KEY_E))
+		cameraNode_->Translate(Vector3::UP * MOVE_SPEED * timeStep);
+	if (input->GetKeyDown(KEY_Q))
+		cameraNode_->Translate(Vector3::DOWN * MOVE_SPEED * timeStep);
+
+    // "Shoot" a physics object with left mousebutton
+    if (input->GetMouseButtonDown(MOUSEB_LEFT))
+        SpawnObject();
+
+    // Check for loading/saving the scene. Save the scene to the file Data/Scenes/Physics.xml relative to the executable
+    // directory
+    if (input->GetKeyPress(KEY_F5))
+    {
+        File saveFile(context_, GetSubsystem<FileSystem>()->GetProgramDir() + "Data/Scenes/Physics.xml", FILE_WRITE);
+        scene_->SaveXML(saveFile);
+    }
+    if (input->GetKeyPress(KEY_F7))
+    {
+        File loadFile(context_, GetSubsystem<FileSystem>()->GetProgramDir() + "Data/Scenes/Physics.xml", FILE_READ);
+        scene_->LoadXML(loadFile);
+    }
+
+    // Toggle physics debug geometry with space
+    if (input->GetKeyPress(KEY_SPACE))
+        drawDebug_ = !drawDebug_;
+}
+
+void Physics::SpawnObject()
+{
+    ResourceCache* cache = GetSubsystem<ResourceCache>();
+
+    // Create a smaller box at camera position
+    Node* boxNode = scene_->CreateChild("SmallBox");
+    boxNode->SetPosition(cameraNode_->GetPosition());
+    boxNode->SetRotation(cameraNode_->GetRotation());
+    boxNode->SetScale(0.25f);
+    StaticModel* boxObject = boxNode->CreateComponent<StaticModel>();
+    boxObject->SetModel(cache->GetResource<Model>("Models/Box.mdl"));
+    boxObject->SetMaterial(cache->GetResource<Material>("Materials/StoneEnvMapSmall.xml"));
+    boxObject->SetCastShadows(true);
+
+    // Create physics components, use a smaller mass also
+    RigidBody* body = boxNode->CreateComponent<RigidBody>();
+    body->SetMass(0.25f);
+    body->SetFriction(0.75f);
+    CollisionShape* shape = boxNode->CreateComponent<CollisionShape>();
+    shape->SetBox(Vector3::ONE);
+
+    const float OBJECT_VELOCITY = 10.0f;
+
+    // Set initial velocity for the RigidBody based on camera forward vector. Add also a slight up component
+    // to overcome gravity better
+    body->SetLinearVelocity(cameraNode_->GetRotation() * Vector3(0.0f, 0.25f, 1.0f) * OBJECT_VELOCITY);
+}
+
+void Physics::HandleUpdate(StringHash eventType, VariantMap& eventData)
+{
+    using namespace Update;
+
+    // Take the frame time step, which is stored as a float
+    float timeStep = eventData[P_TIMESTEP].GetFloat();
+
+    // Move the camera, scale movement with time step
+    MoveCamera(timeStep);
+}
+
+void Physics::HandleKeyDown(StringHash eventType, VariantMap& eventData)
+{
+	int key = eventData[KeyDown::P_KEY].GetInt();
+
+	if (key == KEY_L)
+	{
+		updateLinSuspension_ = !updateLinSuspension_;
+	}
+
+	if (key == KEY_R)
+	{
+		updateRotSuspension_ = !updateRotSuspension_;
+	}
+
+	if (key == KEY_D)
+	{
+		maxHeight_ = 0.0f;
+		k_ = 0.0f;
+		kFactor_ = 0;
+		impulse_ = Vector3::ZERO;
+		maxImpulse_ = 0.0f;
+		timeElapsed_ = 0.0f;
+		// scene_->SetTimeScale(0.1f);
+	}
+
+	if (key == KEY_M)
+	{
+		scene_->SetUpdateEnabled(!scene_->IsUpdateEnabled());
+	}
+
+	if (key == KEY_UP)
+	{
+		kFactor_++;
+		k_ = kFactor_ * 10.0f;
+	}
+
+	if (key == KEY_DOWN)
+	{
+		kFactor_--;
+		k_ = kFactor_ * 10.0f;
+	}
+}
+
+void Physics::HandlePostRenderUpdate(StringHash eventType, VariantMap& eventData)
+{
+    // If draw debug mode is enabled, draw physics debug geometry. Use depth test to make the result easier to interpret
+    if (drawDebug_)
+        scene_->GetComponent<PhysicsWorld>()->DrawDebugGeometry(true);
+
+	if (debugText_)
+	{
+		float posy = body_->GetPosition().y_ - 0.5f;
+		if (posy > maxHeight_)
+			maxHeight_ = posy;
+
+		if (impulse_.y_ > maxImpulse_)
+			maxImpulse_ = impulse_.y_;
+
+		String text;
+		float vel = body_->GetLinearVelocity().y_;
+		text.AppendWithFormat("height <%f>\n max height <%f>\n vel <%f>\n k <%f>\n c <%f>\n error <%f>\n impulse <%f, %f, %f> maximp <%f>", 
+			posy, maxHeight_, vel, k_, c_, floatHeight_ - posy, impulse_.x_, impulse_.y_, impulse_.z_, maxImpulse_);
+		debugText_->SetText(text);
+	}
+}
+
+void Physics::UpdateLineal(float timeStep)
+{
+	// timeElapsed_ += timeStep;
+
+	Quaternion rot = body_->GetRotation();
+	Vector3 pos = body_->GetPosition();
+
+	float posy = pos.y_ - 0.5;
+	float k = k_;
+	float x = floatHeight_ - posy;
+	float springForce = k * x;
+
+	float vel = GetVelocity();
+	float d = 1.0f;
+	float c = -sqrt(mass_ * k * 4) * d;
+	c_ = c;
+
+	float antiG = mass_ * 9.81f;
+	float dampingForce = c * vel;
+	float suspensionForce = springForce + dampingForce + antiG;
+
+	Vector3 direction = Vector3::UP;
+	impulse_ = direction * suspensionForce; // *timeStep;
+
+	body_->ApplyForce(impulse_);
+
+	//URHO3D_LOGERRORF("time, pos <%f, %f>", timeElapsed_, posy);
+}
+
+void Physics::UpdateRotation(float timeStep)
+{
+	timeElapsed_ += timeStep;
+
+	Quaternion rot = body_->GetRotation();
+	Vector3 angVel = body_->GetAngularVelocity();
+
+	float k = 500.0f;
+	float x = (45.0f - rot.EulerAngles().y_) * M_DEGTORAD;
+	float springForce = k * x;
+	// btMatrix3x3 inertia =  hullBody_->GetBody()->getInvInertiaTensorWorld();
+	float d = 1.0f;
+	float moi = mass_ / 12.0f * (width_ * width_ + length_ * length_);
+
+	float c = -sqrt(moi * k * 4.0f) * d;
+	float v = angVel.y_;
+	float dampingForce = c * v;
+
+	float steeringForce = springForce + dampingForce;
+
+	char buff[512];
+	sprintf(buff, "x <%.3f> timestep <%.3f> moi <%.2f>", (45.0f - rot.EulerAngles().y_), timeElapsed_, moi);
+	URHO3D_LOGERRORF("%s", buff);
+
+	Vector3 torqueDir = rot * Vector3::UP;
+	Vector3 impulse = torqueDir * steeringForce;// *timeStep;
+
+	body_->ApplyTorque(impulse);
+}
+
+void Physics::HandlePhysicsPreStep(StringHash eventType, VariantMap& eventData)
+{
+	using namespace PhysicsPreStep;
+	float timeStep = eventData[P_TIMESTEP].GetFloat();
+
+	if (updateLinSuspension_)
+	{
+		UpdateLineal(timeStep);
+	}
+	if (updateRotSuspension_)
+	{
+		UpdateRotation(timeStep);
+	}
+}
+
+float Physics::GetVelocity()
+{
+	Quaternion rot = body_->GetRotation();
+	Vector3 pos = body_->GetPosition();
+
+	Vector3 normal = rot * Vector3::UP;
+	float dd = normal.DotProduct(-Vector3::UP);
+	Vector3 relPos = body_->GetPosition();
+
+	Vector3 contactVel = body_->GetVelocityAtPoint(relPos);
+	float projVel = normal.DotProduct(contactVel);
+	float vel = 0.0f;
+	if (dd >= -0.1f)
+	{
+		vel = 0.0f;
+	}
+	else
+	{
+		float inv = btScalar(-1.) / dd;
+		vel = projVel * inv;
+	}
+
+	return vel;
+}
---
+++ Source/Urho3D/UI/TBElement.h
@@ -0,0 +1,161 @@
+#pragma once
+
+#include "../UI/UIElement.h"
+#include "../Graphics/Texture2D.h"
+#include "../Core/Context.h"
+
+#include "renderers/tb_renderer_batcher.h"
+#include "tb_widgets_common.h"
+#include "tb_message_window.h"
+
+using namespace tb;
+using namespace Urho3D;
+
+//namespace tb
+//{
+//class TBWidgetLister;
+//class TBWidget;
+//class TBBitmap;
+//class TBRendererBatcher;
+//}
+
+namespace Urho3D
+{
+class TBRendererUrho3D;
+class TBUIElement;
+
+static const Urho3D::StringHash E_TBUI_BUTTON_RELEASED("TB_UI_BUTTON_RELEASED");
+static const Urho3D::StringHash E_TBUI_WIDGET_CHANGED("TB_UI_WIDGET_CHANGED");
+
+static const Urho3D::StringHash P_BUTTON_ID("button_id");
+static const Urho3D::StringHash P_BUTTON_TEXT("button_text");
+static const Urho3D::StringHash P_WIDGET_ID("widget_id");
+static const Urho3D::StringHash P_WIDGET_VALUE("widget_value");
+
+class TBRootWidget : public TBWidget, public TBWidgetListener, public Urho3D::Object
+{
+    URHO3D_OBJECT(TBRootWidget, Object)
+public:
+    TBRootWidget(Context* context);
+    
+    virtual bool OnEvent(const TBWidgetEvent &ev);
+    virtual void OnWidgetFocusChanged(TBWidget *widget, bool focused);
+//    virtual void OnResized(int old_w, int old_h);
+};
+
+// TB bitmap for Urho3D engine
+class TBBitmapUrho3D : public TBBitmap
+{
+public:
+    TBBitmapUrho3D(TBRendererUrho3D *renderer);
+    ~TBBitmapUrho3D();
+
+    bool Init(unsigned width, unsigned height, uint32 *data);
+    virtual int Width() { return width_; }
+    virtual int Height() { return height_; }
+    virtual void SetData(uint32 *data);
+
+    SharedPtr<Texture2D> GetTexture() const { return texture_; }
+private:
+    TBRendererUrho3D *renderer_;
+    unsigned width_, height_;
+    SharedPtr<Texture2D> texture_;
+};
+
+// TB renderer for Urho3D engine
+class TBRendererUrho3D : public TBRendererBatcher
+{
+public:
+
+    TBRendererUrho3D();
+    virtual ~TBRendererUrho3D();
+
+    // == TBRenderer ==
+    virtual void BeginPaint(int render_target_w, int render_target_h);
+    virtual void EndPaint();
+
+    virtual TBBitmap *CreateBitmap(int width, int height, uint32 *data);
+
+    // TBRendererBatcher override functions
+    virtual void RenderBatch(TBRendererBatcher::Batch *batch);
+    virtual void SetClipRect(const TBRect &rect);
+
+	unsigned GetBatchSize() const { return batches_.Size(); }
+	UIBatch& GetBatch(unsigned i) { return batches_.At(i); }
+
+	void Clear();
+
+private:
+	// batch vertex data
+    PODVector<float> vertexData_;
+	// batch size - used to access m_vertexData
+    PODVector<UIBatch> batches_;
+	// current clip rect
+    TBRect clipRect_;
+};
+
+// TB UI Element hook to the Urho3D UI System
+// tiene crear el root widget tb y tomar los datos para renderizar 
+class TBUIElement : public UIElement
+{
+   URHO3D_OBJECT(TBUIElement, UIElement)
+
+public:
+    TBUIElement(Context* context);
+    ~TBUIElement();
+
+    static void RegisterObject(Context* context);
+    
+    void AddStateWidget(TBWidget* stateWidget, bool bottom = false, bool fullscreen = true);
+
+    void LoadWidgets(TBWidget* stateWidget, String filename);
+    void LoadResources();
+    void SetBoxSize(int width, int height);
+    void Clear();
+
+    virtual void GetBatches(PODVector<UIBatch>& batches,
+                            PODVector<float>& vertexData,
+                            const IntRect& currentScissor);
+
+    virtual void OnClickBegin (const IntVector2& position,
+                               const IntVector2& screenPosition,
+                               int button, int buttons, int qualifiers,
+                               Cursor* cursor);
+
+    virtual void OnClickEnd (const IntVector2& position,
+                             const IntVector2& screenPosition,
+                             int button, int buttons, int qualifiers,
+                             Cursor* cursor, UIElement* beginElement);
+   
+    // other events
+    virtual void OnWheel(int delta, int buttons, int qualifiers);
+
+    // widget callback
+    virtual void OnPositionSet(const IntVector2& newPosition);
+
+    virtual void Update(float timeStep);
+    virtual bool IsWithinScissor(const IntRect& currentScissor);
+    
+    void HandleBeginFrame(StringHash eventType, VariantMap& eventData);
+    void HandlePostUpdate(StringHash eventType, VariantMap& eventData);
+
+    void HandleScreenMode(StringHash eventType, VariantMap& eventData);
+
+    void HandleKeyDown(StringHash eventType, VariantMap& eventData);
+    void HandleKeyUp(StringHash eventType, VariantMap& eventData);
+	void HandleRawEvent(StringHash eventType, VariantMap& args);
+
+    TBRootWidget* GetRoot() const { return root_; }
+private:
+	
+    void HandleFocused(StringHash /*eventType*/, VariantMap& eventData);
+	void CreateKeyMap();
+	int FindKeyMap(int key);
+
+    TBRootWidget* root_;
+    TBRendererUrho3D* renderer_;
+	HashMap<int, int> mapKey_;
+};
+
+
+}
