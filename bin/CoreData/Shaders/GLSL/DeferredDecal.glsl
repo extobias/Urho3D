92f141f32ffada81cae6734c1b2aba82104947de
extobias <extobias@gmail.com>
2024-09-15 23:16:55 -0300

    clean up

--- bin/CoreData/Shaders/GLSL/DeferredDecal.glsl
+++ bin/CoreData/Shaders/GLSL/DeferredDecal.glsl
@@ -35,18 +35,8 @@ void VS()
 
 void PS()
 {
-    vec2 screenPos = vScreenPos.xy;
-//     vec2 texCoord = vec2(
-//         (1 + screenPos.x) / 2 + 0.5 / resolution.x,
-//         (1 - screenPos.y) / 2 + 0.5 / resolution.y
-//     );
-    vec2 texCoord = vec2(
-        screenPos.x * cGBufferInvSize.x,
-        screenPos.y * cGBufferInvSize.y
-    );
     //Sample a value from the depth buffer
-    // vec4 sampledDepth = texture2D(sDepthBuffer, texCoord);
-    float depth = ReconstructDepth(texture2D(sDepthBuffer, screenPos).r);
+    float depth = ReconstructDepth(texture2D(sDepthBuffer, vScreenPos.xy).r);
     
     vec3 viewRay = vWorldPos.xyz * (cFarClipPS / vWorldPos.z);
     vec3 viewPosition = viewRay * depth;
@@ -54,58 +44,19 @@ void PS()
     vec3 worldPosition = (vec4(viewPosition, 1.0) * vViewInv).xyz;
     vec4 objectPosition = vec4(worldPosition, 1.0) * vModelInv;
     
+    float threshold = 0.5;
     float posAbsX = abs(objectPosition.x);
     float posAbsY = abs(objectPosition.y);
     float posAbsZ = abs(objectPosition.z);
-    if (posAbsX > 0.5 || posAbsY > 0.5 || posAbsZ > 0.5)
+    if (posAbsX > threshold || posAbsY > threshold || posAbsZ > threshold)
         discard;
 
-    vec4 diffColor = cMatDiffColor * texture2D(sDiffMap, vec2(objectPosition.x + 0.5, objectPosition.y + 0.5));
+    float xTexOffset = 0.5;
+    float yTexOffset = 0.5;
+    vec4 diffColor = cMatDiffColor * texture2D(sDiffMap, vec2(objectPosition.x + xTexOffset, objectPosition.y + yTexOffset));
     
     if (diffColor.a < 0.5)
         discard;
         
      gl_FragColor = diffColor;
-    
-    // Get material diffuse albedo
-//     #ifdef DIFFMAP
-//         vec4 diffColor = cMatDiffColor * texture2D(sDiffMap, vTexCoord);
-//         #ifdef ALPHAMASK
-//             if (diffColor.a < 0.5)
-//                 discard;
-//         #endif
-//     #else
-//         vec4 diffColor = cMatDiffColor;
-//     #endif
-// 
-//     #ifdef VERTEXCOLOR
-//         diffColor *= vColor;
-//     #endif
-// 
-//     Get fog factor
-//     #ifdef HEIGHTFOG
-//         float fogFactor = GetHeightFogFactor(vWorldPos.w, vWorldPos.y);
-//     #else
-//         float fogFactor = GetFogFactor(vWorldPos.w);
-//     #endif
-// 
-//     #if defined(PREPASS)
-//         Fill light pre-pass G-Buffer
-//         gl_FragData[0] = vec4(0.5, 0.5, 0.5, 1.0);
-//         gl_FragData[1] = vec4(EncodeDepth(vWorldPos.w), 0.0);
-//     #elif defined(DEFERRED)
-//         gl_FragData[0] = vec4(GetFog(diffColor.rgb, fogFactor), diffColor.a);
-//         gl_FragData[1] = vec4(0.0, 0.0, 0.0, 0.0);
-//         gl_FragData[2] = vec4(0.5, 0.5, 0.5, 1.0);
-//         gl_FragData[3] = vec4(EncodeDepth(vWorldPos.w), 0.0);
-//     #else
-//         gl_FragColor = vec4(GetFog(diffColor.rgb, fogFactor), diffColor.a);
-//     #endif
-    
-//     visualize depthbuffer
-//     float x = vScreenPos.x;
-//     float y = vScreenPos.y;
-//     float depth = DecodeDepth(texture2D(sDepthBuffer, vec2(x, y)).rgb);
-//     vec3 color = vec3(depth, depth, depth);
-//     gl_FragColor = vec4(color, 1.0);
 }
